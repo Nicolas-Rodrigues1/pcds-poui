f18b1164b1051d0558c6397cf33af6e3855787c9
Nicolas-Rodrigues1 <nicolas.rodrigues@ds2u.com.br>
2024-08-02 11:30:45 -0300

    Shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { BannerComponent } from "./banner/banner.component";
+import { CardComponent } from "./card/card.component";
+import { ContainerComponent } from "./container/container.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    declarations:[
+    ],
+    imports:[
+    ],
+    exports:[
+    ]
+})
+
+export class SharedModule{}
