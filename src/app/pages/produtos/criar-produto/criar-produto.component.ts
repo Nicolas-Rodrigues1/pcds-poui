41a0620959f10d2098dc79c0bda9f794472abc0a
Nicolas-Rodrigues1 <nicolas.rodrigues@ds2u.com.br>
2024-08-06 10:42:40 -0300

    Criar Produto

--- src/app/pages/produtos/criar-produto/criar-produto.component.ts
+++ src/app/pages/produtos/criar-produto/criar-produto.component.ts
@@ -1,49 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 import { Router } from '@angular/router';
 import { MensagemService } from 'src/app/core/services/mensagem.service';
 import { Produto } from 'src/app/core/types/type';
 import { ProdutoService } from '../../services/produto.service';
+import { PoDynamicFormComponent, PoDynamicFormField } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-criar-produto',
   templateUrl: './criar-produto.component.html',
   styleUrls: ['./criar-produto.component.scss']
 })
-export class CriarProdutoComponent {
-  
-  produto: Produto = {
+export class CriarProdutoComponent implements OnInit{
+
+  produto = {
     id: 0,
-    categoria: '',
     nome: '',
+    categoria: ''
   }
+
+  validateFields: Array<string> = ['categoria'];
+  
+  fields: Array<PoDynamicFormField> = [
+    {
+      property: 'nome',
+      required: true,
+      icon: 'ph ph-envelope',
+      placeholder: 'Digite o nome do produto'
+    },
+    {
+      property: 'categoria',
+      required: true,
+      options: [
+        {categoria: 'Processador', code: 'processador'},
+        {categoria: 'Placa Mãe', code: 'placaMae'},
+        {categoria: 'Memória Ram', code: 'memoriaRam'},
+        {categoria: 'Armazenamento', code: 'armazenamento'},
+        {categoria: 'Mouse', code: 'mouse'},
+        {categoria: 'Teclado', code: 'teclado'},
+        {categoria: 'Monitor', code: 'monitor'},
+      ],
+      fieldLabel: 'Selecione a categoria',
+      fieldValue: 'code'
+    }
+  ]
 
   constructor(
     private produtoService: ProdutoService,
     private router: Router,
     private mensagemService: MensagemService 
-  ) {
-    this.gerarId();
+  ) {}
+
+  ngOnInit(): void {
+    console.log(this.produto)
   }
 
-  criarProduto() {
-    this.produtoService.criar(this.produto).subscribe(() => {
-      this.router.navigate(['/auth/produtos']);
-    });
-    console.log(this.produto);
-    console.log('Produto criado')
-    // const mensagemProdutoCriado = 'Produto criado com sucesso!';
-    // this.mensagemService.openSnackBar(mensagemProdutoCriado);
+  criarProduto(){
+    const produto = this.produto
+    if (produto.id === 0) {
+      produto.id = Math.floor(Math.random() * 1000000000);
+    }
+    console.log(produto)
+    this.produtoService.criar(produto).subscribe(() => {
+      this.router.navigate(['/pages/produtos'])
+    })
   }
 
   cancelar() {
-    this.router.navigate(['/auth/produtos']);
+    this.router.navigate(['/pages/produtos']);
   }
 
-  gerarId(): void {
-    if (this.produto.id === 0) {
-      this.produto.id = Math.floor(Math.random() * 1000000000);
-    }
-  }
 }
